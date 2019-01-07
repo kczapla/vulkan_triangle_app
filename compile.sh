7e51639fc000c8cba3d30f4e97479bc764e051f2
kczapla <k.czapla68@gmail.com>
2019-01-07 21:22:58 +0100

    recreate swap chain

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 /home/kczapla/lunarg-vulkan/1.1.92.1/x86_64/bin/glslangValidator -V shaders/shader.vert
 /home/kczapla/lunarg-vulkan/1.1.92.1/x86_64/bin/glslangValidator -V shaders/shader.frag
+
+mv ./frag.spv ./shaders/
+mv ./vert.spv ./shaders/
